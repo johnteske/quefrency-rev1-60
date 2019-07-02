26b0c91e687808ccf888f5977f582300e184d2bd
John Teske <john.teske@gmail.com>
2019-07-01 21:02:43 -0700

    Move Fn key

--- keymap.c
+++ keymap.c
@@ -4,11 +4,9 @@ extern keymap_config_t keymap_config;
 
 #define _BASE 0
 #define _FN1 1
-#define _BASE2 2
 
 #define ____ _______
 #define CTRL_ESC LCTL_T(KC_ESC)
-#define LOG_OUT LCA(KC_Q)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -19,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,    KC_2,    KC_3,     KC_4,   KC_5,  KC_6,   KC_7,   KC_8,      KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, \
     KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,  KC_Y,   KC_U,   KC_I,      KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     CTRL_ESC, KC_A,    KC_S,    KC_D,     KC_F,   KC_G,  KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_UP,   LOG_OUT, \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_UP,   MO(_FN1), \
     MO(_FN1), KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,        KC_SPC, KC_SPC, TO(_BASE), TO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -27,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,  KC_F7,  KC_F8,     KC_F9,    KC_F10,  KC_F11,  KC_F12,  ____,    ____, \
     ____,     ____,    ____,    ____,    ____,    ____,  ____,   ____,   ____,       ____,    ____,    ____,    ____,    ____,    \
     ____,     ____,    ____,    ____,    ____,    ____,  KC_LEFT,KC_DOWN,KC_UP,     KC_RGHT,  ____,    ____,    ____,    \
-    ____,     ____,    ____,    ____,    ____,    ____,  ____,   ____,   ____,      ____,     ____,    ____,    ____,    \
+    ____,     ____,    ____,    ____,    ____,    ____,  ____,   ____,   ____,      ____,     ____,    ____,    KC_TRNS, \
     KC_TRNS,  ____,    ____,    ____,    ____,           ____,   ____,   KC_TRNS,   KC_TRNS,  ____,    ____,    ____
   ),
 };
