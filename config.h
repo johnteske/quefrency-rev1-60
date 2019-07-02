0c5695d2612ed6827e4d94ca7976f1d401a9c747
John Teske <john.teske@gmail.com>
2019-07-01 22:17:02 -0700

    Try leader key with logout

--- config.h
+++ config.h
@@ -2,3 +2,6 @@
 
 #define USE_SERIAL
 // #define USE_I2C
+
+#define LEADER_TIMEOUT 300
+//#define LEADER_PER_KEY_TIMING
--- keymap.c
+++ keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,  KC_Y,   KC_U,   KC_I,      KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     CTRL_ESC, KC_A,    KC_S,    KC_D,     KC_F,   KC_G,  KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_UP,   MO(_FN1), \
-    MO(_FN1), KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,        KC_SPC, KC_SPC, TO(_BASE), TO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LEAD,  KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,        KC_SPC, KC_SPC, TO(_BASE), TO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT(
@@ -29,3 +29,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  ____,    ____,    ____,    ____,           ____,   ____,   KC_TRNS,   KC_TRNS,  ____,    ____,    ____
   ),
 };
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_Q) {
+      register_code(KC_LCTRL);
+      register_code(KC_LALT);
+      register_code(KC_Q);
+      unregister_code(KC_Q);
+      unregister_code(KC_LALT);
+      unregister_code(KC_LGUI);
+    }
+  }
+}
