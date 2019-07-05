e4815dc543a263a8ec06c20a243f9f3a60d7c1f7
John Teske <john@98point6.com>
2019-07-05 09:20:46 -0700

    Enable leader key, disable lower/raise
    There may be an unsoldered connection on the raise key.

--- keymap.c
+++ keymap.c
@@ -7,6 +7,7 @@ extern keymap_config_t keymap_config;
 
 #define ____ _______
 #define CTRL_ESC LCTL_T(KC_ESC)
+#define MO_RAISE MO(_FN1)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -17,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,    KC_2,    KC_3,     KC_4,   KC_5,  KC_6,   KC_7,   KC_8,      KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, \
     KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,  KC_Y,   KC_U,   KC_I,      KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     CTRL_ESC, KC_A,    KC_S,    KC_D,     KC_F,   KC_G,  KC_H,   KC_J,   KC_K,      KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_UP,   MO(_FN1), \
-    KC_LEAD,  KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,        KC_SPC, KC_SPC, TO(_BASE), TO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,  KC_N,   KC_M,   KC_COMM,   KC_DOT,   KC_SLSH, KC_UP,   MO_RAISE,\
+    KC_LEAD,  KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,        KC_SPC, KC_SPC, KC_NO,     KC_NO,    KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT(
@@ -38,12 +39,12 @@ void matrix_scan_user(void) {
     leader_end();
 
     SEQ_ONE_KEY(KC_Q) {
-      register_code(KC_LCTRL);
-      register_code(KC_LALT);
+      register_code(KC_LCTL);
+      register_code(KC_LGUI);
       register_code(KC_Q);
       unregister_code(KC_Q);
-      unregister_code(KC_LALT);
       unregister_code(KC_LGUI);
+      unregister_code(KC_LCTL);
     }
   }
 }
